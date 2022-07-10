612b40303a4a917b6ee3ce800dc8eeed740fa549
Gokul R <95705856+gokulr2809@users.noreply.github.com>
2022-07-10 07:23:38 +0530

    Update music_finder.py

--- python/code/utils/music_finder.py
+++ python/code/utils/music_finder.py
@@ -6,17 +6,17 @@ import python.code.config as config
 
 class MusicFinder(object):
     '''Main Class'''
-    def __init__(self, return_api, music_url):
+    def __init__(self, return_api, content):
         '''object of a class is instantiated'''
         self.return_api = return_api
         self.api_token = os.environ['api_token']
-        self.music_url = music_url
+        self.content= content
 
     def finder(self):
         '''finder method and return as json decode'''
         if config.SPOTIFY is True:
             ava = 'spotify'
-            data = {'url': f'{self.music_url}', 'return': f'{ava}',
+            data = {'file': f'{self.content}', 'return': f'{ava}',
                     'api_token': f'{self.api_token}'}
         result = requests.post('https://api.audd.io/', data=data)
         music_data = json.loads(result.text)
